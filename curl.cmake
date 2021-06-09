69f139b0d0103553f5cc647d3f0700fc670c9c14
zouxiaoliang <295919168@qq.com>
2021-06-09 15:36:26 +0800

    修复bug
    1、修复反复导入项目引起的错误

--- curl.cmake
+++ curl.cmake
@@ -2,29 +2,35 @@
 # date: 20210607
 
 if (UNIX)
-
-    if (NOT CMD_MAKE)
-        message(STATUS "[curl] set CMD_MAKE is make.")
-        set(CMD_MAKE make)
-    endif()
-
     set (LIBCURL_PREFIX_DIR "$ENV{HOME}/.nervlab/curl7_77_0-prefix")
     message (STATUS "Using bundled LIBCURL in ${LIBCURL_SRC}")
 
     set(LIBCURL_LIB "${LIBCURL_PREFIX_DIR}/lib/libcurl.a")
     set(LIBCURL_INCLUDE "${LIBCURL_PREFIX_DIR}/include")
-    ExternalProject_Add(
-      libcurl
-      DEPENDS libopenssl
-      URL "https://github.com/curl/curl/releases/download/curl-7_77_0/curl-7.77.0.tar.gz"
-      URL_HASH "SHA256=b0a3428acb60fa59044c4d0baae4e4fc09ae9af1d8a3aa84b2e3fbcd99841f77"
-      CONFIGURE_COMMAND ./configure --prefix=${LIBCURL_PREFIX_DIR} --with-openssl=${LIBOPENSSL_PREFIX_DIR} CFLAGS=-fPIC CPPFLAGS=-fPIC
-      BUILD_COMMAND ${CMD_MAKE}
-      BUILD_IN_SOURCE 1
-      BUILD_BYPRODUCTS ${LIBCURL_LIB}
-      INSTALL_COMMAND ${CMD_MAKE} install
-      UPDATE_COMMAND ""
-    )
+
+    message(STATUS "[curl] CURL_EXISTS is $ENV{CURL_EXISTS}.")
+    if ($ENV{CURL_EXISTS})
+        if (NOT CMD_MAKE)
+            message(STATUS "[curl] set CMD_MAKE is make.")
+            set(CMD_MAKE make)
+        endif()
+
+        set(ENV{CURL_EXISTS} YES)
+        message(STATUS "[curl] set CURL_EXISTS is $ENV{CURL_EXISTS}.")
+
+        ExternalProject_Add(
+            libcurl
+            DEPENDS libopenssl
+            URL "https://github.com/curl/curl/releases/download/curl-7_77_0/curl-7.77.0.tar.gz"
+            URL_HASH "SHA256=b0a3428acb60fa59044c4d0baae4e4fc09ae9af1d8a3aa84b2e3fbcd99841f77"
+            CONFIGURE_COMMAND ./configure --prefix=${LIBCURL_PREFIX_DIR} --with-openssl=${LIBOPENSSL_PREFIX_DIR} CFLAGS=-fPIC CPPFLAGS=-fPIC
+            BUILD_COMMAND ${CMD_MAKE}
+            BUILD_IN_SOURCE 1
+            BUILD_BYPRODUCTS ${LIBCURL_LIB}
+            INSTALL_COMMAND ${CMD_MAKE} install
+            UPDATE_COMMAND ""
+            )
+    endif($ENV{CURL_EXISTS})
 
 endif (UNIX)
 
--- openssl.cmake
+++ openssl.cmake
@@ -2,10 +2,6 @@
 # date: 20210607
 
 if (UNIX)
-    if (NOT CMD_MAKE)
-        message(STATUS "[openssl] set CMD_MAKE is make.")
-        set(CMD_MAKE make)
-    endif()
 
     set (LIBOPENSSL_PREFIX_DIR "$ENV{HOME}/.nervlab/libopenssl_1_1_1k-prefix")
     message (STATUS "Using bundled LIBOPENSSL in ${LIBOPENSSL_SRC}")
@@ -14,15 +10,26 @@ if (UNIX)
     set(LIBOPENSSL_CRYPTO_LIB "${LIBOPENSSL_PREFIX_DIR}/lib/libcrypto.a")
     set(LIBOPENSSL_SSL_LIB "${LIBOPENSSL_PREFIX_DIR}/lib/libssl.a")
 
-    ExternalProject_Add(
-        libopenssl
-        URL "https://www.openssl.org/source/openssl-1.1.1k.tar.gz"
-        URL_HASH "SHA256=892a0875b9872acd04a9fde79b1f943075d5ea162415de3047c327df33fbaee5"
-        CONFIGURE_COMMAND ./config --prefix=${LIBOPENSSL_PREFIX_DIR} no-asm CFLAGS=-fPIC CPPFLAGS=-fPIC
-        BUILD_COMMAND ${CMD_MAKE}
-        BUILD_IN_SOURCE 1
-        BUILD_BYPRODUCTS ${LIBOPENSSL_LIB}
-        INSTALL_COMMAND ${CMD_MAKE} install
-        UPDATE_COMMAND ""
-        )
+    message(STATUS "[openssl] OPENSSL_EXISTS is $ENV{OPENSSL_EXISTS}.")
+    if ($ENV{OPENSSL_EXISTS})
+        if (NOT CMD_MAKE)
+            message(STATUS "[openssl] set CMD_MAKE is make.")
+            set(CMD_MAKE make)
+        endif()
+
+        set(ENV{OPENSSL_EXISTS} YES)
+        message(STATUS "[openssl] set OPENSSL_EXISTS is $ENV{OPENSSL_EXISTS}.")
+
+        ExternalProject_Add(
+            libopenssl
+            URL "https://www.openssl.org/source/openssl-1.1.1k.tar.gz"
+            URL_HASH "SHA256=892a0875b9872acd04a9fde79b1f943075d5ea162415de3047c327df33fbaee5"
+            CONFIGURE_COMMAND ./config --prefix=${LIBOPENSSL_PREFIX_DIR} no-asm CFLAGS=-fPIC CPPFLAGS=-fPIC
+            BUILD_COMMAND ${CMD_MAKE}
+            BUILD_IN_SOURCE 1
+            BUILD_BYPRODUCTS ${LIBOPENSSL_LIB}
+            INSTALL_COMMAND ${CMD_MAKE} install
+            UPDATE_COMMAND ""
+            )
+    endif($ENV{OPENSSL_EXISTS})
 endif (UNIX)
