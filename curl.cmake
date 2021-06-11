b5740bbde0aaad04041af037fefcbc91aa151cd2
zouxiaoliang <295919168@qq.com>
2021-06-11 11:36:22 +0800

    修复bug
    1、去掉唯一性判断，因唯一性判断在你重复导入时，导致项目依赖出错
    2、最小化curl库的依赖库

--- curl.cmake
+++ curl.cmake
@@ -8,29 +8,26 @@ if (UNIX)
     set(LIBCURL_LIB "${LIBCURL_PREFIX_DIR}/lib/libcurl.a")
     set(LIBCURL_INCLUDE "${LIBCURL_PREFIX_DIR}/include")
 
-    message(STATUS "[curl] CURL_EXISTS is $ENV{CURL_EXISTS}.")
-    if ($ENV{CURL_EXISTS})
-        if (NOT CMD_MAKE)
-            message(STATUS "[curl] set CMD_MAKE is make.")
-            set(CMD_MAKE make)
-        endif()
+    if (NOT CMD_MAKE)
+        message(STATUS "[curl] set CMD_MAKE is make.")
+        set(CMD_MAKE make)
+    endif()
 
-        set(ENV{CURL_EXISTS} YES)
-        message(STATUS "[curl] set CURL_EXISTS is $ENV{CURL_EXISTS}.")
+    message(STATUS "[curl] add external project: curl")
 
-        ExternalProject_Add(
-            libcurl
-            DEPENDS libopenssl
-            URL "https://github.com/curl/curl/releases/download/curl-7_77_0/curl-7.77.0.tar.gz"
-            URL_HASH "SHA256=b0a3428acb60fa59044c4d0baae4e4fc09ae9af1d8a3aa84b2e3fbcd99841f77"
-            CONFIGURE_COMMAND ./configure --prefix=${LIBCURL_PREFIX_DIR} --with-openssl=${LIBOPENSSL_PREFIX_DIR} CFLAGS=-fPIC CPPFLAGS=-fPIC
-            BUILD_COMMAND ${CMD_MAKE}
-            BUILD_IN_SOURCE 1
-            BUILD_BYPRODUCTS ${LIBCURL_LIB}
-            INSTALL_COMMAND ${CMD_MAKE} install
-            UPDATE_COMMAND ""
-            )
-    endif($ENV{CURL_EXISTS})
+    # 依赖库 https://curl.se/docs/libs.html
+    ExternalProject_Add(
+        curl
+        DEPENDS openssl
+        URL "https://github.com/curl/curl/releases/download/curl-7_77_0/curl-7.77.0.tar.gz"
+        URL_HASH "SHA256=b0a3428acb60fa59044c4d0baae4e4fc09ae9af1d8a3aa84b2e3fbcd99841f77"
+        CONFIGURE_COMMAND ./configure --prefix=${LIBCURL_PREFIX_DIR} --without-librtmp --without-libidn2 --without-zstd --without-brotli --without-nghttp2 --without-nghttp3 --without-ldap-lib --with-openssl=${LIBOPENSSL_PREFIX_DIR} CFLAGS=-fPIC CPPFLAGS=-fPIC
+        BUILD_COMMAND ${CMD_MAKE}
+        BUILD_IN_SOURCE 1
+        BUILD_BYPRODUCTS ${LIBCURL_LIB}
+        INSTALL_COMMAND ${CMD_MAKE} install
+        UPDATE_COMMAND ""
+        )
 
 endif (UNIX)
 
--- openssl.cmake
+++ openssl.cmake
@@ -10,26 +10,23 @@ if (UNIX)
     set(LIBOPENSSL_CRYPTO_LIB "${LIBOPENSSL_PREFIX_DIR}/lib/libcrypto.a")
     set(LIBOPENSSL_SSL_LIB "${LIBOPENSSL_PREFIX_DIR}/lib/libssl.a")
 
-    message(STATUS "[openssl] OPENSSL_EXISTS is $ENV{OPENSSL_EXISTS}.")
-    if ($ENV{OPENSSL_EXISTS})
-        if (NOT CMD_MAKE)
-            message(STATUS "[openssl] set CMD_MAKE is make.")
-            set(CMD_MAKE make)
-        endif()
+    if (NOT CMD_MAKE)
+        message(STATUS "[openssl] set CMD_MAKE is make.")
+        set(CMD_MAKE make)
+    endif()
 
-        set(ENV{OPENSSL_EXISTS} YES)
-        message(STATUS "[openssl] set OPENSSL_EXISTS is $ENV{OPENSSL_EXISTS}.")
+    message(STATUS "[curl] add external project: openssl")
 
-        ExternalProject_Add(
-            libopenssl
-            URL "https://www.openssl.org/source/openssl-1.1.1k.tar.gz"
-            URL_HASH "SHA256=892a0875b9872acd04a9fde79b1f943075d5ea162415de3047c327df33fbaee5"
-            CONFIGURE_COMMAND ./config --prefix=${LIBOPENSSL_PREFIX_DIR} no-asm CFLAGS=-fPIC CPPFLAGS=-fPIC
-            BUILD_COMMAND ${CMD_MAKE}
-            BUILD_IN_SOURCE 1
-            BUILD_BYPRODUCTS ${LIBOPENSSL_LIB}
-            INSTALL_COMMAND ${CMD_MAKE} install
-            UPDATE_COMMAND ""
-            )
-    endif($ENV{OPENSSL_EXISTS})
+    ExternalProject_Add(
+        openssl
+        URL "https://www.openssl.org/source/openssl-1.1.1k.tar.gz"
+        URL_HASH "SHA256=892a0875b9872acd04a9fde79b1f943075d5ea162415de3047c327df33fbaee5"
+        CONFIGURE_COMMAND ./config --prefix=${LIBOPENSSL_PREFIX_DIR} no-asm CFLAGS=-fPIC CPPFLAGS=-fPIC
+        BUILD_COMMAND ${CMD_MAKE}
+        BUILD_IN_SOURCE 1
+        BUILD_BYPRODUCTS ${LIBOPENSSL_LIB}
+        INSTALL_COMMAND ${CMD_MAKE} install
+        UPDATE_COMMAND ""
+        )
+
 endif (UNIX)
